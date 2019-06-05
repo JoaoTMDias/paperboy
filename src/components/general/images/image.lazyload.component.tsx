46e157897822696d4a2c0f70bb0d4526b2f9f2dd
João Dias <>
2019-06-05 16:10:10 +0100

    Lazy Loading

--- src/components/general/images/image.lazyload.component.tsx
+++ src/components/general/images/image.lazyload.component.tsx
@@ -5,7 +5,9 @@ import styled, { keyframes, css } from 'styled-components';
 
 // Component Prop
 
-import { IImageLazyProps } from '../../../data/interfaces/index.interface';
+import { IImageLazyProps, EImageType, IImageSourceString, IPictureSources } from '../../../data/interfaces/index.interface';
+
+const DEFAULT_BASELINE_VALUE = 16;
 
 /**
  * @description Lazy Loading Image Component
@@ -13,35 +15,42 @@ import { IImageLazyProps } from '../../../data/interfaces/index.interface';
  * @date  29/December/2018 at 01:17
  * @extends {React.FunctionComponent}
  */
-class LazyLoadingImage extends React.Component<IImageLazyProps> {
+class LazyLoadingImage extends React.PureComponent<IImageLazyProps> {
 	static defaultProps = {
-		width: `100`,
-		height: `100`,
+		type: EImageType.IMAGE,
+		useNativeLazyLoading: false,
+		source: null,
+		width: `200`,
+		height: `200`,
+		loading: 'lazy',
 		placeholderColor: `transparent`,
 		animation: `fade`,
-		debounce: 250,
+		debounce: 200,
 	};
 
 	/**
-	 * @description The component only re-renders if the width, height, cover or alt are changed
-	 * Otherwise, it remains the same.
-	 * @date 2019-01-17
-	 * @param {IImageLazyProps} nextProps
-	 * @param {*} nextState
+	 * @description Renders the Sources Elements
+	 * @author João Dias
+	 * @date 2019-06-03
+	 * @param {IPictureSources[]} sources
 	 * @returns
 	 * @memberof LazyLoadingImage
 	 */
-	shoudComponentUpdate(nextProps: IImageLazyProps, nextState: any) {
-		const { width, height, src, alt } = this.props;
-		if (
-			nextProps.width !== width ||
-			nextProps.height !== height ||
-			nextProps.src !== src ||
-			nextProps.alt !== alt
-		) {
-			return true;
-		}
-		return false;
+	renderPictureSources(sources: IPictureSources[]) {
+		const pictureSources = sources.map(({ srcSet, media }, index: number) => {
+			const mediaValue = `(${media.type}: ${media.breakpoint * DEFAULT_BASELINE_VALUE}rem)`;
+			let arrayOfSrcSet: string[] = [];
+
+			srcSet.map((set: IImageSourceString) => {
+				arrayOfSrcSet.push(`${set.url} ${set.density}x`);
+			});
+
+			return (
+				<source key={`source-${index}-key`} srcSet={arrayOfSrcSet.join()} media={mediaValue} />
+			);
+		});
+
+		return pictureSources;
 	}
 
 	/**
@@ -50,9 +59,12 @@ class LazyLoadingImage extends React.Component<IImageLazyProps> {
 	render() {
 		const {
 			id,
+			useNativeLazyLoading,
 			className,
 			width,
 			height,
+			type,
+			sources,
 			src,
 			srcSet,
 			sizes,
@@ -67,6 +79,29 @@ class LazyLoadingImage extends React.Component<IImageLazyProps> {
 			onError,
 		} = this.props;
 
+		if (useNativeLazyLoading) {
+			return (
+				<Image
+					id={id}
+					className={className}
+					src={src}
+					srcSet={srcSet}
+					sizes={sizes}
+					width={width}
+					height={height}
+					loading="lazy"
+					alt={alt}
+					placeholderColor={placeholderColor}
+					animation={animation}
+					fit={fit}
+					positionTop={positionTop}
+					positionLeft={positionLeft}
+					onLoad={onLoad}
+					onError={onError}
+				/>
+			);
+		}
+
 		return (
 			<LazyImage
 				src={src}
@@ -74,27 +109,80 @@ class LazyLoadingImage extends React.Component<IImageLazyProps> {
 				sizes={sizes}
 				alt={alt}
 				debounceDurationMs={debounce}
-				placeholder={({ imageProps, ref }) => (
-					<Placeholder
-						id={id}
-						className={className}
-						ref={ref}
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 24 24"
-						preserveAspectRatio="xMaxYMax slice"
-						role="img"
-						width={width}
-						height={height}
-					>
-						<title id="placeholder-title">{imageProps.alt}</title>
-						<path
-							className="placeholder__background"
-							fill={placeholderColor}
-							d="M0 0h24v24H0z"
-						/>
-					</Placeholder>
-				)}
+				placeholder={({ imageProps, ref }) => {
+					return (
+						<Placeholder
+							id={id}
+							className={`${className} LazyImage-Placeholder`}
+							ref={ref}
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							preserveAspectRatio="xMaxYMax slice"
+							role="img"
+							width={width}
+							height={height}
+						>
+							<title id="placeholder-title">{imageProps.alt}</title>
+							<path
+								className="placeholder__background"
+								fill={placeholderColor}
+								d="M0 0h24v24H0z"
+							/>
+						</Placeholder>
+					)
+				}}
+				loading={() => {
+					return (
+						<Placeholder
+							id={id}
+							className={`${className} LazyImage-Placeholder`}
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							preserveAspectRatio="xMaxYMax slice"
+							role="img"
+							width={width}
+							height={height}
+						>
+							<path
+								className="placeholder__background"
+								fill={placeholderColor}
+								d="M0 0h24v24H0z"
+							/>
+						</Placeholder>
+					)
+				}}
 				actual={() => {
+					if (type === EImageType.PICTURE && sources) {
+						const pictureSources = this.renderPictureSources(sources);
+
+						return (
+							<Picture
+								style={{
+									width,
+									height
+								}}
+							>
+								{pictureSources}
+								<Image
+									id={id}
+									className={className}
+									src={src}
+									srcSet={srcSet}
+									sizes={sizes}
+									width={width}
+									height={height}
+									loading="lazy"
+									alt={alt}
+									placeholderColor={placeholderColor}
+									animation={animation}
+									fit={fit}
+									positionTop={positionTop}
+									positionLeft={positionLeft}
+									onLoad={onLoad} />
+							</Picture>
+						);
+					}
+
 					return (
 						<Image
 							id={id}
@@ -104,6 +192,7 @@ class LazyLoadingImage extends React.Component<IImageLazyProps> {
 							sizes={sizes}
 							width={width}
 							height={height}
+							loading="lazy"
 							alt={alt}
 							placeholderColor={placeholderColor}
 							animation={animation}
@@ -123,13 +212,11 @@ class LazyLoadingImage extends React.Component<IImageLazyProps> {
 // Styling
 const FadeInImage = keyframes`
 	from {
-		opacity: 0;
-		filter: grayscale(100%);
+		opacity: 0.1;
 	}
 
 	to {
 		opacity: 1;
-		filter: grayscale(0%);
 	}
 `;
 
@@ -137,49 +224,51 @@ const ZoomImage = keyframes`
 	0% {
 		opacity: 0;
 		transform: scale(1);
-		filter: grayscale(100%);
 	}
 
 	6.6% {
 		opacity: 1;
-		filter: grayscale(0%);
 
 	}
 
 	100% {
 		opacity: 1;
 		transform: scale(1.1);
-		filter: grayscale(0%);
 	}
 `;
 
 const Image = styled.img`
 	background-color: ${(props: IImageLazyProps) =>
 		props.placeholderColor ? `${props.placeholderColor}` : `transparent`};
-	transform: scale(1);
 
 	${(props: IImageLazyProps) => {
-		const { animation } = props;
+		const { animation, speed } = props;
 
 		if (animation) {
+			const duration = speed ? `${speed}ms` : '128ms';
 			switch (animation) {
+				case `zoom`:
+					return css`
+						opacity: 0;
+						transform: scale(1);
+
+						animation-fill-mode: forwards;
+						animation-name: ${ZoomImage};
+						animation-duration: 15000ms;
+					`;
+
+				default:
 				case `fade`:
 					return css`
 						opacity: 0;
 						animation-fill-mode: forwards;
 						animation-name: ${FadeInImage};
-						animation-duration: 500ms;
-					`;
-
-				case `zoom`:
-					return css`
-						opacity: 0;
-						animation-fill-mode: forwards;
-						animation-name: ${ZoomImage};
-						animation-duration: 15000ms;
+						animation-duration: ${`${duration}`};
 					`;
 			}
 		}
+
+		return null;
 	}};
 
 	object-fit: ${(props: IImageLazyProps) => {
@@ -190,8 +279,17 @@ const Image = styled.img`
 		return `cover`;
 	}};
 
-	object-position: ${(props: IImageLazyProps) =>
-		`${props.positionTop} ${props.positionLeft}`};
+	object-position: ${(props: IImageLazyProps) => `${props.positionTop} ${props.positionLeft}`};
+`;
+
+const Picture = styled.picture`
+	width: 100%;
+	height: auto;
+	position: relative;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
 `;
 
 const Placeholder = styled.svg`
--- src/data/interfaces/lazy-image.interface.ts
+++ src/data/interfaces/lazy-image.interface.ts
@@ -1,5 +1,28 @@
+export enum EImageType {
+	IMAGE = 'IMAGE',
+	PICTURE = 'PICTURE',
+};
+
+export interface IImageSourceString {
+	url: string,
+	density: number,
+};
+
+export interface ISourceMedia {
+	type: string,
+	breakpoint: number,
+};
+
+export interface IPictureSources {
+	srcSet: IImageSourceString[],
+	media: ISourceMedia,
+}
+
 export interface IImageLazyProps {
 	id?: string;
+	useNativeLazyLoading?: boolean;
+	type?: EImageType;
+	sources?: IPictureSources[] | null;
 	className?: string;
 	width?: string;
 	height?: string;
@@ -9,7 +32,9 @@ export interface IImageLazyProps {
 	alt: string;
 	debounce?: number;
 	placeholderColor?: string;
+	loading?: 'lazy' | 'eager' | 'auto';
 	animation?: 'fade' | 'zoom' | null;
+	speed?: number;
 	style?: React.CSSProperties;
 	fit?: string;
 	positionTop?: string;
