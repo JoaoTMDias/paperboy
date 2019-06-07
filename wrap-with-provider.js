5b80c7ff4bb3653816d8fb2a0e2ea086862d28c5
João Dias <>
2019-06-07 15:49:01 +0100

    no message

--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-
 import { store, persistor } from './src/data/redux/store/index';
+import { UIContentSpinner } from './src/components';
 
 // eslint-disable-next-line
 export default ({ element }) => (
 	<Provider store={store}>
-		<PersistGate persistor={persistor} loading={<div>Loading...</div>}>
+		<PersistGate
+			persistor={persistor}
+			loading={
+				<UIContentSpinner
+					isFullPage
+					center
+					color="var(--color-primary)"
+				/>
+			}
+		>
 			{element}
 		</PersistGate>
 	</Provider>
